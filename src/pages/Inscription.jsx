3bec634265a44d99f9ae4060ca6edaf16c33ae71
MarieConstance <mgoa90334@gmail.com>
2023-10-28 18:08:51 +0200

    login and register

--- src/pages/Inscription.jsx
+++ src/pages/Inscription.jsx
@@ -1,52 +1,34 @@
 import "./../assets/css/Inscription.css";
-import google from "./../assets/images/goog.png";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+
+
 function Inscription() {
   return (
-    <div className="Inscription">
-      <form>
-        <h3>Foumulaire d&apos; Inscription</h3>
-        <button className="google">
-          <img src={google} alt="" />
-          <p>S&apos;incrire avec votre compte Coogle</p>
-        </button>
-        <div className="content__or-text">
-          <span></span>
-          <span>Ou</span>
-          <span></span>
-        </div>
-        <div className="labInp">
-          <div className="mb-6">
-            <label htmlFor="email">Adress Email</label>
-            <input
-              type="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Mot de passe
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="**********"
-              required
-            />
-          </div>
-        </div>
-        <div className="flex justify-center">
-          <button className=" text-white py-3 px-2 mt-2 rounded items-center m-10">
-            Incris
+    <div className="coprs-contenaire">
+    <div className="container">
+    <div className="heading">Inscription</div>
+    <form action="" className="form">
+    <input required="" className="input" type="text" name="nom" id="nom" placeholder="Nom d'Utilisateur"/>
+      <input required="" className="input" type="email" name="email" id="email" placeholder="Adresse Email"/>
+      <input required="" className="input" type="password" name="password" id="password" placeholder="Mot de Passe"/>
+      <span className="forgot-password"><a href="#">Mot de passe oublié ?</a></span>
+      <input className="login-button" type="submit" value="S'inscrire"/>
+      
+    </form>
+    <div className="social-account-container">
+        <span className="title">Ou connectez-vous avec</span>
+        <div className="social-accounts">
+          <Link>
+          <button className="social-button google">
+            <span>{<FcGoogle/>}</span>
           </button>
+          </Link>
+
         </div>
-      </form>
-    </div>
+      </div>
+  </div>
+  </div>
   );
 }
 
