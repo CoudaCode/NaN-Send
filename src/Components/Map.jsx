379b5baaee748cd017a4fb94870d0c91b819dba4
Diarassouba Mamadou <couda.dm@gmail.com>
2023-10-31 08:57:46 +0000

    ok

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,108 +1,85 @@
-import React from "react";
 import "./../assets/css/Map.css";
+import { FaCalendar, FaPhone, FaEnvelope, FaAddressCard } from "react-icons/fa";
+
 function Map() {
   return (
-    <div className="Map">
-      <div className="frame">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.093516214048!2d-3.966303815471035!3d5.402723021204745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfc19316175b8f2b%3A0xf1efa3cf6347ba24!2sPharmacie%20Analya!5e0!3m2!1sfr!2sci!4v1696264972412!5m2!1sfr!2sci"
-          width="90%"
-          style={{ height: "70vh", margin: "3vw" }}
-          allowFullScreen=""
-          loading="lazy"
-        ></iframe>
-        <div className="contact">
-          <h1>CONTACT INFORMATION</h1>
-          {/* <div>
-        <ul>
-          <li>
-            <a href="" className="fa-solid fa-phone"></a>Telephone
-            <p>fff</p>
-          </li>
-          <li>
-          <a href="" className="fa-solid fa-phone"></a>Telephone
-            <p>fff</p>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <ul>
-          <li>
-          <a href="" className="fa-solid fa-phone"></a>Telephone
-            <p>fff</p>
-          </li>
-          <li>
-          <a href="" className="fa-solid fa-phone"></a>Telephone
-            <p>fff</p>
-          </li>
-        </ul>
-      </div> */}
-          <div className="row">
-            <div className="col">
-              <a href="">
-                <i className="fa-solid fa-phone"></i>
-              </a>{" "}
-              <a
-                href=""
-                className="mesLien"
-                style={{ fontSize: "25px", textDecoration: "none" }}
-              >
-                {" "}
-                Telephone
-              </a>
-              <p>+2250789252529</p>
-            </div>
-            <div className="col">
-              <a href="">
-                <i className="fa-solid fa-envelopes-bulk"></i>
-              </a>
-              <a
-                href=""
-                className="mesLien"
-                style={{ fontSize: "25px", textDecoration: "none" }}
-              >
-                {" "}
-                Mail
-              </a>
-              <p>info@nan.ci</p>
-            </div>
-            <div className="w-100"></div>
-            <div className="col">
-              <a href="">
-                <i className="fa-solid fa-location-dot"></i>
-              </a>
-              <a
-                href=""
-                className="mesLien"
-                style={{ fontSize: "25px", textDecoration: "none" }}
-              >
-                {" "}
-                Adresse
-              </a>
-              <p>
-                Abidjan, Cocody Angré 8éme <br /> Tranche GESTOCI <br />
-                (Non loin de la cité DIASPORA)
-              </p>
-            </div>
-            <div className="col">
-              <a href="">
-                <i className="fa-regular fa-calendar-check"></i>
-              </a>
-              <a
-                href=""
-                className="mesLien"
-                style={{ fontSize: "25px", textDecoration: "none" }}
-              >
-                {" "}
-                Temps de Travail
-              </a>
-              <p>
-                Lundi Au Vendredi <br /> De 08h00 à 17h00
-              </p>
+    <div className="Map container my-12 py-12 mx-auto px-4 md:px-6 lg:px-12 Map">
+      <section className="mb-20 text-gray-800">
+        <h1 className="text-3xl font-bold text-center mb-4">
+          Contactez-Nous...
+        </h1>
+        <div className="md:flex md:flex-wrap">
+          <div className="xl:w-6/12 md:w-7/12 mb-6 lg:mb-0 md:pr-3">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15888.404091335024!2d-3.962697!3d5.401578!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfc1933ec546f809%3A0xa13e690552d16e30!2sNaN%20Digital%20Academy!5e0!3m2!1sfr!2sci!4v1698668060502!5m2!1sfr!2sci"
+              className="h-80 w-full border-0 rounded-lg shadow-lg"
+              allowFullScreen=""
+              loading="lazy"></iframe>
+          </div>
+          <div className="xl:w-6/12 md:w-5/12 md:pl-3 cadre rounded-lg">
+            <div className="grid xl:grid-cols-2 xl:grid-rows-2 xl:gap-x-4 md:gap-x-6">
+              <div className="mb-1 md:mb-12">
+                <div className="flex items-start">
+                  <div className="shrink-0">
+                    <div className="p-2 rounded-md w-14 h-14 flex items-center justify-center Icon">
+                      <FaPhone />
+                    </div>
+                  </div>
+                  <div>
+                    <h3 className="font-bold mb-1 text-white">Telephone</h3>
+                    <p className="text-gray-500">+2250789252529</p>
+                  </div>
+                </div>
+              </div>
+              <div className="mb-1 md:mb-12">
+                <div className="flex items-start">
+                  <div className="shrink-0">
+                    <div className="p-2 rounded-md w-14 h-14 flex items-center justify-center Icon">
+                      <FaEnvelope />
+                    </div>
+                  </div>
+                  <div>
+                    <h3 className="font-bold mb-1 text-white">Mail</h3>
+                    <p className="text-gray-500">info@nan.ci</p>
+                  </div>
+                </div>
+              </div>
+              <div className="mb-1 md:mb-12">
+                <div className="flex align-start">
+                  <div className="shrink-0">
+                    <div className="p-2 rounded-md w-14 h-14 flex items-center justify-center Icon">
+                      <FaAddressCard />
+                    </div>
+                  </div>
+                  <div>
+                    <h3 className="font-bold mb-1 text-white">Adresse</h3>
+                    <p className="text-gray-500">
+                      Abidjan, Cocody Angré 8ème Tranche GESTOCI (Non loin de la
+                      cité DIASPORA)
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="mb-4 md:mb-4">
+                <div className="flex align-start">
+                  <div className="shrink-0">
+                    <div className="p-2 rounded-md w-14 h-14 flex items-center justify-center Icon">
+                      <FaCalendar />
+                    </div>
+                  </div>
+                  <div>
+                    <h3 className="font-bold mb-0.5 text-white">
+                      Temps de travail
+                    </h3>
+                    <p className="text-gray-500">Du Lundi au Vendredi</p>
+                    <p className="text-gray-500">De 08h a 17h</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
--- src/pages/Connexion.jsx
+++ src/pages/Connexion.jsx
@@ -1,37 +1,95 @@
-import React from "react";
 import "./../assets/css/Connexion.css";
-import google from "./../assets/images/goog.png";
-
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Cookie from "js-cookie";
+import axios from "axios";
 function Connexion() {
+  const navigate = useNavigate();
+
+  const login = async (data) => {
+    const result = await axios.post(
+      "https://nan-send-api.onrender.com/api/user/login",
+      data
+    );
+    return result;
+  };
+
+  const { register, handleSubmit } = useForm({
+    email: "",
+    password: "",
+  });
+
+  const { mutate: loginUser } = useMutation({
+    mutationFn: (Mydata) => login(Mydata),
+    onSuccess: (succes) => {
+      console.log("demo", succes.data);
+      toast.success(succes.data.message);
+      Cookie.set("token", succes.data.token, { expires: 3600 * 24 });
+      setTimeout(() => {
+        sessionStorage.setItem("token", JSON.stringify(succes.data.id));
+        navigate("/dashbord");
+      }, 3000);
+    },
+    onError: (e) => {
+      console.log(e.response.data.message);
+      toast.error(e.response.data.message);
+    },
+  });
+
+  const onSubmit = (data) => loginUser(data);
   return (
-    <div className="Connexion">
-      <form>
-        <div className="text">
-          <h3>Formulaire de connexion</h3>
-          <button className="google">
-            <img src={google} alt="" />
-            <p>connecter-vous avec votre compte Coogle </p>
-          </button>
-          <div className="content__or-text">
-            <span></span>
-            <span>Ou</span>
-            <span></span>
+    <>
+      <div className="Connexion">
+        <div className="container">
+          <div className="heading">Connexion</div>
+          <form action="" className="form" onSubmit={handleSubmit(onSubmit)}>
+            <input
+              {...register("email", {
+                required: "svp votre  Email",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "svp entrer un mail valide",
+                },
+              })}
+              className="input"
+              type="email"
+              id="email"
+              placeholder="Adresse Email"
+            />
+            <input
+              required=""
+              className="input"
+              type="password"
+              id="password"
+              placeholder="Mot de Passe"
+              {...register("password", { required: true })}
+            />
+            <span className="forgot-password">
+              <a href="#">Mot de passe oublié ?</a>
+            </span>
+            <input
+              className="login-button"
+              type="submit"
+              value="Se connecter"
+            />
+          </form>
+          <div className="social-account-container">
+            <span className="title">Ou connectez-vous avec</span>
+            <div className="social-accounts">
+              <Link>
+                <button className="social-button google">
+                  <span>{<FcGoogle />}</span>
+                </button>
+              </Link>
+            </div>
           </div>
-          <label htmlFor="email">Adresse Email</label>
-          <br />
-          <input type="text" id="email" />
-          <br />
-          <label htmlFor="mdp">Mot De Passe</label>
-          <br />
-          <input type="text" id="mdp" />
-          <br />
-          <button type="submit" className="btnconnexion">
-            Connexion
-          </button>
         </div>
-      </form>
-    </div>
+      </div>
+    </>
   );
 }
-
 export default Connexion;
