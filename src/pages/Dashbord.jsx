de12e26519395919b45f1a58b4dbbaf4ba8e4f96
kouame konan abraham <abrahamkonany@gmail.com>
2023-10-31 07:12:08 +0000

    tache non terminer

--- src/pages/Dashbord.jsx
+++ src/pages/Dashbord.jsx
@@ -11,160 +11,98 @@ function Dashbord() {
         <div className="row flex-nowrap">
           <SideBar />
           <div className="col py-3 cardRassemblement">
-          <div className="flex flex-col justify-center items-center h-[50vh] pt-4">
-            <div className="min-w-[375px] md:min-w-[700px] xl:min-w-[800px] mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-6">
-                <div className="relative flex flex-grow !flex-row flex-col items-center rounded-[10px] rounded-[10px] border-[1px] border-gray-200 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
-                    <div className="ml-[18px] flex h-[90px] w-auto flex-row items-center">
-                    <div className="rounded-full bg-lightPrimary p-3 dark:bg-navy-700">
-                        <span className="flex items-center text-brand-500 dark:text-white">
+          <div className="container-fluid mt-4">
+                <div className="grid grid-cols-1 gap-4 px-4 mt-8 sm:grid-cols-4 sm:px-8 md:grid-cols-2  md:px-8 ">
+                    <div className="flex items-center bg-white border rounded-sm overflow-hidden shadow">
+                        <div className="p-4 bg-green-400">
                         <svg
-                            stroke="currentColor"
-                            fill="currentColor"
-                            stroke-width="0"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-12 w-12 text-white"
+                            fill="none"
                             viewBox="0 0 24 24"
-                            className="h-7 w-7"
-                            height="1em"
-                            width="1em"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path fill="none" d="M0 0h24v24H0z"></path>
-                            <path d="M4 9h4v11H4zM16 13h4v7h-4zM10 4h4v16h-4z"></path>
-                        </svg>
-                        </span>
-                    </div>
-                    </div>
-                    <div className="h-50 ml-4 flex w-auto flex-col justify-center">
-                    <p className="font-dm text-sm font-medium text-gray-600">Earnings</p>
-                    <h4 className="text-xl font-bold text-navy-700 dark:text-white">$340.5</h4>
-                    </div>
-                </div>
-                <div className="relative flex flex-grow !flex-row flex-col items-center rounded-[10px] rounded-[10px] border-[1px] border-gray-200 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
-                    <div className="ml-[18px] flex h-[90px] w-auto flex-row items-center">
-                    <div className="rounded-full bg-lightPrimary p-3 dark:bg-navy-700">
-                        <span className="flex items-center text-brand-500 dark:text-white">
-                        <svg
                             stroke="currentColor"
-                            fill="currentColor"
-                            stroke-width="0"
-                            viewBox="0 0 512 512"
-                            className="h-6 w-6"
-                            height="1em"
-                            width="1em"
-                            xmlns="http://www.w3.org/2000/svg"
                         >
-                            <path d="M298.39 248a4 4 0 002.86-6.8l-78.4-79.72a4 4 0 00-6.85 2.81V236a12 12 0 0012 12z"></path>
-                            <path d="M197 267a43.67 43.67 0 01-13-31v-92h-72a64.19 64.19 0 00-64 64v224a64 64 0 0064 64h144a64 64 0 0064-64V280h-92a43.61 43.61 0 01-31-13zm175-147h70.39a4 4 0 002.86-6.8l-78.4-79.72a4 4 0 00-6.85 2.81V108a12 12 0 0012 12z"></path>
-                            <path d="M372 152a44.34 44.34 0 01-44-44V16H220a60.07 60.07 0 00-60 60v36h42.12A40.81 40.81 0 01231 124.14l109.16 111a41.11 41.11 0 0111.83 29V400h53.05c32.51 0 58.95-26.92 58.95-60V152z"></path>
+                            <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+                            ></path>
                         </svg>
-                        </span>
+                        </div>
+                        <div className="px-4 text-gray-700">
+                        <h3 className="text-sm tracking-wider">Total Teams</h3>
+                        <p className="text-3xl">12,768</p>
+                        </div>
                     </div>
-                    </div>
-                    <div className="h-50 ml-4 flex w-auto flex-col justify-center">
-                    <p className="font-dm text-sm font-medium text-gray-600">Spend this month</p>
-                    <h4 className="text-xl font-bold text-navy-700 dark:text-white">$642.39</h4>
-                    </div>
-                </div>
-                <div className="relative flex flex-grow !flex-row flex-col items-center rounded-[10px] rounded-[10px] border-[1px] border-gray-200 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
-                    <div className="ml-[18px] flex h-[90px] w-auto flex-row items-center">
-                    <div className="rounded-full bg-lightPrimary p-3 dark:bg-navy-700">
-                        <span className="flex items-center text-brand-500 dark:text-white">
+                    <div className="flex items-center bg-white border rounded-sm overflow-hidden shadow">
+                        <div className="p-4 bg-blue-400">
                         <svg
-                            stroke="currentColor"
-                            fill="currentColor"
-                            stroke-width="0"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-12 w-12 text-white"
+                            fill="none"
                             viewBox="0 0 24 24"
-                            className="h-7 w-7"
-                            height="1em"
-                            width="1em"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path fill="none" d="M0 0h24v24H0z"></path>
-                            <path d="M4 9h4v11H4zM16 13h4v7h-4zM10 4h4v16h-4z"></path>
-                        </svg>
-                        </span>
-                    </div>
-                    </div>
-                    <div className="h-50 ml-4 flex w-auto flex-col justify-center">
-                    <p className="font-dm text-sm font-medium text-gray-600">Sales</p>
-                    <h4 className="text-xl font-bold text-navy-700 dark:text-gray-600 ">$574.34</h4>
-                    </div>
-                </div>
-                <div className="relative flex flex-grow !flex-row flex-col items-center rounded-[10px] rounded-[10px] border-[1px] border-gray-200 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
-                    <div className="ml-[18px] flex h-[90px] w-auto flex-row items-center">
-                    <div className="rounded-full bg-lightPrimary p-3 dark:bg-navy-700">
-                        <span className="flex items-center text-brand-500 dark:text-white">
-                        <svg
                             stroke="currentColor"
-                            fill="currentColor"
-                            stroke-width="0"
+                        >
+                            <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
+                            ></path>
+                        </svg>
+                        </div>
+                        <div className="px-4 text-gray-700">
+                        <h3 className="text-sm tracking-wider">Total Contact</h3>
+                        <p className="text-3xl">39,265</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center bg-white border rounded-sm overflow-hidden shadow">
+                        <div className="p-4 bg-indigo-400">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-12 w-12 text-white"
+                            fill="none"
                             viewBox="0 0 24 24"
-                            className="h-6 w-6"
-                            height="1em"
-                            width="1em"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path fill="none" d="M0 0h24v24H0z"></path>
-                            <path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"></path>
-                        </svg>
-                        </span>
-                    </div>
-                    </div>
-                    <div className="h-50 ml-4 flex w-auto flex-col justify-center">
-                    <p className="font-dm text-sm font-medium text-gray-600">Your Balance</p>
-                    <h4 className="text-xl font-bold text-navy-700 dark:text-white">$1,000</h4>
-                    </div>
-                </div>
-                <div className="relative flex flex-grow !flex-row flex-col items-center rounded-[10px] rounded-[10px] border-[1px] border-gray-200 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
-                    <div className="ml-[18px] flex h-[90px] w-auto flex-row items-center">
-                    <div className="rounded-full bg-lightPrimary p-3 dark:bg-navy-700">
-                        <span className="flex items-center text-brand-500 dark:text-white">
-                        <svg
                             stroke="currentColor"
-                            fill="currentColor"
-                            stroke-width="0"
+                        >
+                            <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
+                            ></path>
+                        </svg>
+                        </div>
+                        <div className="px-4 text-gray-700">
+                        <h3 className="text-sm tracking-wider">Total Message</h3>
+                        <p className="text-3xl">142,334</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center bg-white border rounded-sm overflow-hidden shadow">
+                        <div className="p-4 bg-red-400">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-12 w-12 text-white"
+                            fill="none"
                             viewBox="0 0 24 24"
-                            className="h-7 w-7"
-                            height="1em"
-                            width="1em"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path fill="none" d="M0 0h24v24H0z"></path>
-                            <path d="M4 9h4v11H4zM16 13h4v7h-4zM10 4h4v16h-4z"></path>
-                        </svg>
-                        </span>
-                    </div>
-                    </div>
-                    <div className="h-50 ml-4 flex w-auto flex-col justify-center">
-                    <p className="font-dm text-sm font-medium text-gray-600">New Tasks</p>
-                    <h4 className="text-xl font-bold text-navy-700 dark:text-white">145</h4>
-                    </div>
-                </div>
-                <div className="relative flex flex-grow !flex-row flex-col items-center rounded-[10px] rounded-[10px] border-[1px] border-gray-200 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
-                    <div className="ml-[18px] flex h-[90px] w-auto flex-row items-center">
-                    <div className="rounded-full bg-lightPrimary p-3 dark:bg-navy-700">
-                        <span className="flex items-center text-brand-500 dark:text-white">
-                        <svg
                             stroke="currentColor"
-                            fill="currentColor"
-                            stroke-width="0"
-                            viewBox="0 0 512 512"
-                            className="h-6 w-6"
-                            height="1em"
-                            width="1em"
-                            xmlns="http://www.w3.org/2000/svg"
                         >
-                            <path d="M208 448V320h96v128h97.6V256H464L256 64 48 256h62.4v192z"></path>
+                            <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
+                            ></path>
                         </svg>
-                        </span>
-                    </div>
-                    </div>
-                    <div className="h-50 ml-4 flex w-auto flex-col justify-center">
-                    <p className="font-dm text-sm font-medium text-gray-600">Total Projects</p>
-                    <h4 className="text-xl font-bold text-navy-700 dark:text-white">$2433</h4>
+                        </div>
+                        <div className="px-4 text-gray-700">
+                        <h3 className="text-sm tracking-wider">Server Load</h3>
+                        <p className="text-3xl">34.12%</p>
+                        </div>
                     </div>
                 </div>
-                </div> 
-        </div>
+            </div>
           </div>
         </div>
       </div>
