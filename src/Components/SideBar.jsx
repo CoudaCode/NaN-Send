861a211d1dbc7f2e9148d96bcbcbfaeb3e818077
Diarassouba Mamadou <couda.dm@gmail.com>
2023-10-31 07:55:23 +0000

    ok

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,5 +1,5 @@
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-
 import {
   FaBorderAll,
   FaUsers,
@@ -13,26 +13,20 @@ import {
 
 function SideBar() {
   const path = useLocation().pathname;
+  const [active, setActive] = useState(path);
 
   const sidebarLinks = [
-    { path: "/dashbord", text: "Dashboard", icon: <FaBorderAll /> },
-
+    { path: "/dashboard", text: "Dashboard", icon: <FaBorderAll /> },
     { path: "/contact", text: "Contacts", icon: <FaUsers /> },
-
     { path: "/message", text: "Messages", icon: <FaClipboardList /> },
-
     { path: "/broadcasts", text: "Broadcasts", icon: <FaCalendar /> },
-
     { path: "/profile", text: "Profiles", icon: <FaUserCircle /> },
-
     {
       path: "/workspace",
       text: "Workspace setting",
       icon: <FaHourglassStart />,
     },
-
     { path: "/reports", text: "Reports", icon: <FaComments /> },
-
     { path: "/", text: "Déconnexion", icon: <FaArrowLeft /> },
   ];
 
@@ -44,7 +38,6 @@ function SideBar() {
           className="text-violet-500 text-decoration-none pb-3 mb-sm-0 me-sm-auto">
           <span className="text-lg hidden sm:inline">Menu</span>
         </Link>
-
         <ul
           className="nav nav-pills flex-col mb-sm-0 mb-0 sm:items-start"
           id="menu">
@@ -52,34 +45,30 @@ function SideBar() {
             <li key={link.path} className="nav-item">
               <Link
                 to={link.path}
-                className={`nav-link align-middle px-0 ${
+                className={`nav-link align-middle px-0 w-44 ${
                   path === link.path
                     ? "bg-purple-500 text-blue-900"
                     : "hover:bg-purple-200 text-violet-700"
                 }`}>
-                <span className="flex items-center">
+                <div className="flex items-center">
                   {link.icon}
-
                   <span
                     className={`ml-2 hidden sm:inline ${
                       path === link.path ? "text-red-300" : "text-gray-700"
                     }`}>
                     {link.text}
                   </span>
-                </span>
+                </div>
               </Link>
             </li>
           ))}
         </ul>
-
         <hr />
       </div>
-
       <div
         className="dropdown rounded-4 m-2 p-4 bg-white absolute bottom-0 start-0"
         id="btnBottom">
         <h5>NAN DIGITAL ACADEMY</h5>
-
         <h6>Version: 1.0.0.11</h6>
       </div>
     </div>
